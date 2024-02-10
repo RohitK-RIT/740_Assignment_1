3a8c764bdf9dc437ef75cf4d31f65ab502fae564
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2024-02-09 22:07:05 -0500

    Update Paint.cpp
    little optimizations

--- Paint.cpp
+++ Paint.cpp
@@ -22,24 +22,31 @@ struct object
 	const float* point_size;
 };
 
+constexpr float small_point = 1.0f, medium_point = 5.0f, large_point = 10.0f;
+constexpr color red = {1.0f, 0.0f, 0.0f}, green = {0.0f, 1.0f, 0.0f}, blue = {0.0f, 0.0f, 1.0f};
+
 float canvas_size[] = {10.0f, 10.0f};
 int raster_size[] = {800, 600};
 
 float mouse_pos[2];
 int total_vertices;
-object current_object;
-vector<object> completed_objects;
+object* current_object;
+vector<object*> objects;
 
-constexpr float small_point = 1.0f, medium_point = 5.0f, large_point = 10.0f;
-constexpr color red = {1.0f, 0.0f, 0.0f}, green = {0.0f, 1.0f, 0.0f}, blue = {0.0f, 0.0f, 1.0f};
+void create_new_object(GLenum draw_type, const color* color, const float* point_size)
+{
+	objects.push_back(new object());
+	current_object = objects.back();
+	current_object->vertices.clear();
+	current_object->draw_type = draw_type;
+	current_object->color = color;
+	current_object->point_size = point_size;
+}
 
 void init()
 {
 	mouse_pos[0] = mouse_pos[1] = 0.0f;
-	current_object.vertices.clear();
-	current_object.draw_type = GL_LINE_LOOP;
-	current_object.color = &red;
-	current_object. point_size = &small_point;
+	create_new_object(GL_LINE_LOOP, &red, &small_point);
 	total_vertices = 3;
 }
 
@@ -62,46 +69,39 @@ void display()
 	glLoadIdentity();
 
 	// Draw completed objects
-	for (const object& object : completed_objects)
+	for (const object* object : objects)
 	{
-		glColor3f(object.color->r, object.color->g, object.color->b);
-		glPointSize(*object.point_size);
-		glBegin(object.draw_type);
-		for (const vertex& vertex : object.vertices)
+		glColor3f(object->color->r, object->color->g, object->color->b);
+		glPointSize(*object->point_size);
+		glBegin(object->draw_type);
+		if (current_object == object)
 		{
-			glVertex2f(vertex.x, vertex.y);
-		}
-		glEnd();
-		glPointSize(*current_object.point_size);
-	}
-
-	// Draw current object
-	if (!current_object.vertices.empty())
-	{
-		glColor3f(current_object.color->r, current_object.color->g, current_object.color->b);
-		glPointSize(*current_object.point_size);
-		glBegin(current_object.draw_type);
-		if (total_vertices == 2)
-		{
-			if (!current_object.vertices.empty())
+			if (total_vertices == 2)
 			{
-				const auto first_vertex = current_object.vertices[0];
-				glVertex2f(first_vertex.x, first_vertex.y);
-				glVertex2f(first_vertex.x, mouse_pos[1]);
+				if (!current_object->vertices.empty())
+				{
+					const auto first_vertex = current_object->vertices[0];
+					glVertex2f(first_vertex.x, first_vertex.y);
+					glVertex2f(first_vertex.x, mouse_pos[1]);
+					glVertex2fv(mouse_pos);
+					glVertex2f(mouse_pos[0], first_vertex.y);
+				}
+			}
+			else
+			{
+				for (const vertex& vertex : current_object->vertices)
+					glVertex2f(vertex.x, vertex.y);
+
 				glVertex2fv(mouse_pos);
-				glVertex2f(mouse_pos[0], first_vertex.y);
 			}
 		}
 		else
 		{
-			for (const vertex& vertex : current_object.vertices)
-			{
+			for (const vertex& vertex : object->vertices)
 				glVertex2f(vertex.x, vertex.y);
-			}
-			glVertex2fv(mouse_pos);
 		}
 		glEnd();
-		glPointSize(*current_object.point_size);
+		glPointSize(*current_object->point_size);
 	}
 
 	drawCursor();
@@ -115,32 +115,31 @@ void mouse(int button, int state, int x, int y)
 		mouse_pos[0] = static_cast<float>(x) / raster_size[0] * canvas_size[0];
 		mouse_pos[1] = static_cast<float>(raster_size[1] - y) / raster_size[1] * canvas_size[1];
 
-		current_object.vertices.push_back({mouse_pos[0], mouse_pos[1]});
+		current_object->vertices.push_back({mouse_pos[0], mouse_pos[1]});
 
-		if (static_cast<int>(current_object.vertices.size()) == total_vertices)
+		if (static_cast<int>(current_object->vertices.size()) == total_vertices)
 		{
-			const auto temp = current_object.draw_type;
+			const auto temp = current_object->draw_type;
 			switch (total_vertices)
 			{
 			case 2:
-				current_object.draw_type = GL_QUADS;
-				const auto first_vertex = current_object.vertices[0];
-				const auto second_vertex = current_object.vertices[1];
-				current_object.vertices.insert(current_object.vertices.begin() + 1, {{first_vertex.x, second_vertex.y}});
-				current_object.vertices.push_back({second_vertex.x, first_vertex.y});
+				current_object->draw_type = GL_QUADS;
+				const auto first_vertex = current_object->vertices[0];
+				const auto second_vertex = current_object->vertices[1];
+				current_object->vertices.insert(current_object->vertices.begin() + 1, {{first_vertex.x, second_vertex.y}});
+				current_object->vertices.push_back({second_vertex.x, first_vertex.y});
 				break;
 			case 3:
-				current_object.draw_type = GL_TRIANGLES;
+				current_object->draw_type = GL_TRIANGLES;
 				break;
 			case -2:
-				current_object.draw_type = GL_POLYGON;
+				current_object->draw_type = GL_POLYGON;
 				break;
 			default:
 				break;
 			}
-			completed_objects.push_back(std::move(current_object));
-			current_object.vertices.clear();
-			current_object.draw_type = temp;
+
+			create_new_object(temp, current_object->color, current_object->point_size);
 		}
 
 		glutPostRedisplay();
@@ -162,28 +161,28 @@ void reshape(int w, int h)
 
 void try_store_object()
 {
-	if (static_cast<int>(current_object.vertices.size()) != total_vertices && total_vertices != -2 && total_vertices != -1)
+	if (static_cast<int>(current_object->vertices.size()) != total_vertices && total_vertices != -2 && total_vertices != -1)
 	{
-		current_object.vertices.clear();
+		current_object->vertices.clear();
 		return;
 	}
 
 	switch (total_vertices)
 	{
 	case 2:
-		current_object.draw_type = GL_QUADS;
+		current_object->draw_type = GL_QUADS;
 		break;
 	case 3:
-		current_object.draw_type = GL_TRIANGLES;
+		current_object->draw_type = GL_TRIANGLES;
 		break;
 	case -2:
-		current_object.draw_type = GL_POLYGON;
+		current_object->draw_type = GL_POLYGON;
 		break;
 	default:
 		break;
 	}
-	completed_objects.push_back(std::move(current_object));
-	current_object.vertices.clear();
+
+	create_new_object(current_object->draw_type, current_object->color, current_object->point_size);
 }
 
 void motion(int x, int y)
@@ -213,8 +212,8 @@ void menu(int value)
 	{
 	// main menu
 	case 0: // clear
-		current_object.vertices.clear();
-		completed_objects.clear();
+		current_object->vertices.clear();
+		objects.clear();
 		glutPostRedisplay();
 		break;
 	case 1: //exit
@@ -224,59 +223,59 @@ void menu(int value)
 	case 5: // point
 		try_store_object();
 		total_vertices = 1;
-		current_object.draw_type = GL_POINTS;
+		current_object->draw_type = GL_POINTS;
 		glutPostRedisplay();
 		break;
 	case 6: // line
 		try_store_object();
 		total_vertices = -1;
-		current_object.draw_type = GL_LINE_STRIP;
+		current_object->draw_type = GL_LINE_STRIP;
 		glutPostRedisplay();
 		break;
 	case 7: // triangle
 		try_store_object();
 		total_vertices = 3;
-		current_object.draw_type = GL_LINE_LOOP;
+		current_object->draw_type = GL_LINE_LOOP;
 		glutPostRedisplay();
 		break;
 	case 8: // quad
 		try_store_object();
 		total_vertices = 2;
-		current_object.draw_type = GL_LINE_LOOP;
+		current_object->draw_type = GL_LINE_LOOP;
 		glutPostRedisplay();
 		break;
 	case 9: // polygon
 		try_store_object();
 		total_vertices = -2;
-		current_object.draw_type = GL_LINE_LOOP;
+		current_object->draw_type = GL_LINE_LOOP;
 		glutPostRedisplay();
 		break;
 
 	// color selection
 	case 10: // red
-		current_object.color = &red;
+		current_object->color = &red;
 		glutPostRedisplay();
 		break;
 	case 11: // green
-		current_object.color = &green;
+		current_object->color = &green;
 		glutPostRedisplay();
 		break;
 	case 12: // blue
-		current_object.color = &blue;
+		current_object->color = &blue;
 		glutPostRedisplay();
 		break;
 
 	// point size selection
 	case 13: // small
-		current_object.point_size = &small_point;
+		current_object->point_size = &small_point;
 		glutPostRedisplay();
 		break;
 	case 14: // medium
-		current_object.point_size = &medium_point;
+		current_object->point_size = &medium_point;
 		glutPostRedisplay();
 		break;
 	case 15: // large
-		current_object.point_size = &large_point;
+		current_object->point_size = &large_point;
 		glutPostRedisplay();
 		break;
 
